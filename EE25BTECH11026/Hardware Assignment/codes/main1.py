0d2aa378347fee0410c04a5dc763b302805d5670
HarshaBJ2007 <bjharsha2007@gmail.com>
2025-10-20 18:10:46 +0530

    updated on 20/10/25(2)

--- EE25BTECH11026/Hardware Assignment/codes/main1.py
+++ EE25BTECH11026/Hardware Assignment/codes/main1.py
@@ -1,5 +1,7 @@
+# This code gives the coefficients for f(t)
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
 
 A = np.loadtxt('training_data.txt')
 T_train = A[:, 0]
@@ -9,7 +11,8 @@ X_train = np.column_stack((np.ones(A.shape[0]), T_train, T_train**2))
 
 theta = np.linalg.lstsq(X_train, V_train)[0].reshape(-1, 1)
 print("The value of n is:")
-print(theta)
+theta=sp.Array(theta)
+sp.pprint(theta)
 
 plt.figure()
 plt.plot(T_train, X_train @ theta, label='Fitted Model')
@@ -19,7 +22,7 @@ plt.ylabel('Output Voltage (V)')
 plt.xlabel(r'Temperature ($^{\degree}$C)') 
 plt.legend()
 plt.tight_layout()
-plt.savefig('/home/user/Matrix Theory: workspace/Hardware Assignment/figs/train.png')
+plt.savefig('/home/user/Matrix Theory: workspace/Hardware Assignment/figs/voltage_vs_temp/train.png')
 plt.show()
 plt.close()
 
@@ -38,6 +41,6 @@ plt.ylabel('Output Voltage (V)')
 plt.xlabel(r'Temperature ($^{\degree}$C)') 
 plt.legend()
 plt.tight_layout()
-plt.savefig('/home/user/Matrix Theory: workspace/Hardware Assignment/figs/valid.png')
+plt.savefig('/home/user/Matrix Theory: workspace/Hardware Assignment/figs/voltage_vs_temp/valid.png')
 plt.show()
 
